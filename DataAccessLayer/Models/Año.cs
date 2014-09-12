a04cc3a48ee4c7c7747e3667fb7a561b17a88dea
manoel <manoelruizmorales@gmail.com>
2014-09-12 12:09:57 -0500

    Se agrego estructura para verificacion de obra en planeacion

--- DataAccessLayer/Models/Año.cs
+++ DataAccessLayer/Models/Año.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace DataAccessLayer.Models
 {
     public class Año:Generica
     {
-        public int Anio { get; set; }
-        public bool Activo { get; set; }
+        [Index(IsUnique = true)]
+        public int Anio { get; set; }        
     }
 }
--- DataAccessLayer/Models/Ejercicio.cs
+++ DataAccessLayer/Models/Ejercicio.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace DataAccessLayer.Models
 {
     public class Ejercicio:Generica
     {
+        [Index(IsUnique = true)]
         public int Año { get; set; }
+        public bool Activo { get; set; }
     }
 }
