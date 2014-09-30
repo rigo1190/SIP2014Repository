4d4ed3f37f8f28bf2148473b231f11ed9f797954
manoel <manoelruizmorales@gmail.com>
2014-09-30 14:33:04 -0500

    Agregar clases para el PVD

---
+++ DataAccessLayer/Models/PlanSectorial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class PlanSectorial:Generica
+    {
+        public PlanSectorial() 
+        {
+            this.DetalleSubElementos = new HashSet<PlanSectorial>();
+        }
+
+        [Index(IsUnique = true)]
+        [StringLength(50, ErrorMessage = "El campo {0} debe contener un máximo de {1} caracteres")]
+        public string Clave { get; set; }       
+        public string Descripcion { get; set; }
+
+        [Index("IX_Orden_ParentId", 1, IsUnique = true)]
+        public int Orden { get; set; }
+
+        [Index("IX_Orden_ParentId", 2)]
+        public int? ParentId { get; set; }
+        public virtual Eje Parent { get; set; }
+        public virtual ICollection<PlanSectorial> DetalleSubElementos { get; set; }
+    }
+}
